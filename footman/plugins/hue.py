60a52a60875711b351e90cf39d287eef291b234e
Max Vitek <maxvitek@gmail.com>
2014-03-08 10:45:42 -0600

    remove comments

--- footman/plugins/hue.py
+++ footman/plugins/hue.py
@@ -112,28 +112,3 @@ class HuePlugin(IPlugin):
         from footman.plugins.voice import VoicePlugin
         self.voice = VoicePlugin()
         return None
-
-
-# # get the commandline arguments
-# cmd = str(sys.argv)
-# targetLight = sys.argv[1]
-# targetCmd = sys.argv[2]
-#
-# # network setup
-# hueURL = 'http://' + HUE_IP + '/api/' + HUE_USER + '/lights/'
-#
-# if targetCmd == 'off':
-#     reqData = "{\"on\":false}"
-# elif targetCmd == 'on':
-#     reqData = "{\"on\":true, \"sat\":35, \"bri\":150,\"hue\":15000,\"effect\":\"none\"}"
-# elif targetCmd == 'crazy':
-#     reqData = "{\"on\":true, \"sat\":255, \"bri\":255,\"hue\":0,\"effect\":\"colorloop\"}"
-# elif targetCmd == 'bright':
-#     reqData = "{\"on\":true, \"sat\":35, \"bri\":255,\"hue\":15000,\"effect\":\"none\"}"
-# elif targetCmd == 'dim':
-#     reqData = "{\"on\":true, \"sat\":35, \"bri\":20,\"hue\":15000,\"effect\":\"none\"}"
-# else:
-#     reqData = "{}"
-#
-# requests.put(hueURL + targetLight + '/state',
-#              data=reqData)
